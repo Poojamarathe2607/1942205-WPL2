703e9ee30cf07e0f3901bc16a797ceedc610e64d
Poojamarathe2607 <82228680+Poojamarathe2607@users.noreply.github.com>
2021-04-10 21:38:59 +0530

    Update views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def show(request):
-    return HttpResponse('Hello World!')
+    return HttpResponse('Hello My name is Pooja Sanjay Marathe from batch T4 and my PRN is 1942205')
